63a183101405d2dab57d8a1f8f9ea1d66a52ef29
Eoin Brazil <eoin.brazil@gmail.com>
2014-01-28 18:37:24 +0000

    Added initial outline and update docs.

---
+++ tamethekraken.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const Workshopper = require('workshopper')
+    , path        = require('path')
+
+Workshopper({
+    name     : 'tamethekraken'
+  , title    : 'TAME THE KRAKEN!'
+  , appDir   : __dirname
+  , helpFile : path.join(__dirname, 'help.txt')
+  , prerequisitesFile : path.join(__dirname, 'prerequisites.txt')
+  , creditsFile : path.join(__dirname, 'credits.txt')
+}).init()
